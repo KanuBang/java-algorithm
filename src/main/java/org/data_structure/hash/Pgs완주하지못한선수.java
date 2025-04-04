af9b3d9c8f780e4fe932d76f450b1bb0deef8e41
gdrffg <gdrffg@gmail.com>
2025-04-04 13:33:48 +0900

    pgs 완주하지못한선수 update

--- src/main/java/org/data_structure/hash/Pgs완주하지못한선수.java
+++ src/main/java/org/data_structure/hash/Pgs완주하지못한선수.java
@@ -8,28 +8,21 @@ public class Pgs완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        // 1. participant로 완주자: 사람수 HashMap을 만든다.
+        // 1. participant로 완주자:사람수 HashMap을 만든다.
         HashMap<String,Integer> players = new HashMap<>();
         for(String name: participant){
-            if(players.containsKey(name)){
-                players.put(name, players.get(name)+1);
-            }
-            else {
-                players.put(name, 1);
-            }
+            players.put(name, players.getOrDefault(name,0) + 1);
         }
 
-        // 2. completion으로 HashMap 완주자:사람수 를 채운다 완주자가 있으면 사람수를 -1 한다.
+        // 2. completion으로 완주자가 있으면 사람수를 -1 한다.
         for(String name: completion){
-            if(players.containsKey(name)){
-                players.put(name, players.get(name)-1);
-            }
+            players.put(name, players.get(name)-1);
         }
 
-        // 3. 최종적으로 사람수가 1 이상인 value가 답이다.
+        // 3. 최종적으로 사람수가 0이 아니면 value가 답이다.
         Set<String> keySet = players.keySet();
         for(String key: keySet){
-            if(players.get(key) >= 1){
+            if(players.get(key) != 0){
                 answer += key;
                 break;
             }
